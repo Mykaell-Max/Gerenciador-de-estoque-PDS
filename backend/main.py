33f149ec45e3f4420284a80bc4ea62710235de28
ImprovedSS <improvedss@hotmail.com>
2026-07-05 22:48:57 -0300

    Scrum-6 Finalizado

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,20 @@ from fastapi.middleware.cors import CORSMiddleware #Biblioteca para permitir a c
 from pydantic import BaseModel #Biblioteca para formatar os dados recebidos automaticamente
 import psycopg2 #Biblioteca de conexão com o banco de dados
 from backend.init_db import inicializar_banco
+from backend.conexao import conectar
+from datetime import date
 
 inicializar_banco()
 
 class DadosLogin(BaseModel):
     usuario: str
     senha: str
+
+class DadosCadastro(BaseModel):
+    usuario: str
+    email: str
+    senha: str
+    tipo: str
 
 app = FastAPI()
 
@@ -20,8 +28,121 @@ app.add_middleware(
         allow_headers=["*"],
 )
 
+conn = conectar("estoquedb")
+cursor = conn.cursor()
+cursor.execute("SET search_path TO Banco")
+conn.commit()
+
 
 @app.post("/login")
 def realizarLogin(dados: DadosLogin):
-    return {"message": "Dados recebidos com sucesso!"}
+    if(dados.usuario.strip() == "" or len(dados.usuario) < 3):
+        return {
+            "sucess": False,
+            "message": "Insira um usuário válido."
+        }
+
+    if(dados.senha.strip() == "" or len(dados.senha) < 8):
+        return {
+            "sucess": False,
+            "message": "Insira uma senha válida."
+        }
+
+    cursor.execute(
+        "SELECT * FROM Usuario WHERE nome = %s and senha = %s",
+        (dados.usuario, dados.senha)
+    )
+
+    linha = cursor.fetchone()
+    if linha is None:
+        return {
+            "sucess": False,
+            "message": "Usuario inexistente ou senha incorreta"
+        }
+
+    return {
+        "sucess": True,
+        "tipo": str(linha[4])
+    }
+
+
+@app.post("/cadastrar")
+def realizarCadastro(dados: DadosCadastro):
+    if dados.usuario.strip() == "":
+        return {
+            "sucess": False,
+            "message": "Usuário não pode estar vazio."
+        }
+    
+    if len(dados.usuario) < 3:
+        return {
+            "sucess": False,
+            "message": "Usuário deve ter no mínimo 3 caracteres."
+        }
+    
+    if dados.email.strip() == "":
+        return {
+            "sucess": False,
+            "message": "Email não pode estar vazio."
+        }
+        
+    if "@" not in dados.email or ".com" not in dados.email:
+        return {
+            "sucess": False,
+            "message": "Email em um formato inválido."
+        }
+
+    if dados.senha.strip() == "":
+        return {
+            "sucess": False,
+            "message": "Senha não pode estar vazia."
+        }
+    
+    if len(dados.senha) < 8:
+        return {
+            "sucess": False,
+            "message": "Senha deve ter no mínimo 8 caracteres."
+        }
+
+    if dados.tipo not in {"A", "C"}:
+        return {
+            "sucess": False,
+            "message": "Tipo inválido."
+        }
+
+    cursor.execute(
+        "SELECT 1 FROM Usuario WHERE nome = %s",
+        (dados.usuario,)
+    )
+
+    if cursor.fetchone():
+        return {
+            "sucess": False,
+            "message": "Usuário já cadastrado."
+        }
+
+    cursor.execute(
+        "SELECT 1 FROM Usuario WHERE email = %s",
+        (dados.email,)
+    )
+
+    if cursor.fetchone():
+        return {
+            "sucess": False,
+            "message": "Email já cadastrado."
+        }
+
+    data_atual = date.today()
+
+    cursor.execute(
+        "INSERT INTO Usuario(nome, senha, email, tipo, data_inicio) VALUES (%s, %s, %s, %s, %s)",
+        (dados.usuario, dados.senha, dados.email, dados.tipo, data_atual)
+    )
+    conn.commit()
+
+    return {
+        "sucess": True,
+        "message": "Cadastro realizado com sucesso"
+    }
+
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,20 @@ function App() {
   const [usuario, setUsuario] = useState("")
   const [senha, setSenha] = useState("")
   const [logado, setLogado] = useState(false)
+  const [tipo, setTipo] = useState("")
+  const [message, setMessage] = useState("")
 
   async function realizarLogin() {
+    if(usuario.trim() == "" || usuario.length < 3) {
+      setMessage("Insira um usuário válido.")
+      return
+    }
+
+    if(senha.trim == "" || senha.length < 8) {
+      setMessage("Insira uma senha válida.")
+      return
+    }
+
     const dados = {
       usuario: usuario,
       senha: senha
@@ -24,7 +36,15 @@ function App() {
     })
     const dadosResposta = await resposta.json()
     console.log(dadosResposta)
-    setLogado(true)
+    if(dadosResposta.sucess) {
+      setLogado(true)
+      setTipo(dadosResposta.tipo)
+      setUsuario("")
+      setSenha("")
+      setMessage("")
+    } else {
+      setMessage(dadosResposta.message)
+    }
   }
 
   function alteraUser(evento) {
@@ -36,9 +56,12 @@ function App() {
   }
 
   if(logado) {
-    return (<TelaAdmin></TelaAdmin>)
-  }
-
+    if(tipo == "A") {
+      return <TelaAdmin setLogado={setLogado}/>
+    }
+    return <TelaUsuario setLogado={setLogado}/>
+  } 
+  
   return (
     <>
 
@@ -62,16 +85,21 @@ function App() {
             <button type="button" onClick={realizarLogin}>Entrar</button>
           </form>
         </div>
+        <hr />
+        <div>
+          <p>{message}</p>
+        </div>
       </section>
     </>
   )
 }
 
-function TelaAdmin() {
+function TelaAdmin(props) {
   const [usuario, setUsuario] = useState("")
   const [email, setEmail] = useState("")
   const [senha, setSenha] = useState("")
   const [tipo, setTipo] = useState("C")
+  const [message, setMessage] = useState("")
 
   function alteraUsuario(evento) {
     setUsuario(evento.target.value)
@@ -89,13 +117,60 @@ function TelaAdmin() {
     setTipo(evento.target.value)
   }
 
-  function realizaCadastro() {
-    setUsuario("")
-    setSenha("")
-    setEmail("")
-    setTipo("C")
-  }
+  async function realizaCadastro() {
+    if(usuario.trim() == "") {
+      setMessage("Usuário não pode estar vazio.")
+      return
+    }
 
+    if(usuario.length < 3) {
+      setMessage("Usuário deve ter no mínimo 3 caracteres.")
+      return
+    }
+
+    if(email.trim() == "") {
+      setMessage("Email não pode estar vazio.")
+      return
+    }
+
+    if(!email.includes("@") || !email.includes(".com")) {
+      setMessage("Email em um formato inválido.")
+      return
+    }
+
+    if(senha.trim() == "") {
+      setMessage("Senha não pode estar vazia.")
+      return
+    }
+
+    if(senha.length < 8) {
+      setMessage("Senha deve ter no mínimo 8 caracteres.")
+      return
+    }
+  
+    const dados = {
+      usuario,
+      email,
+      senha,
+      tipo
+    }
+    const resposta = await fetch("http://127.0.0.1:8000/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dados)
+    })
+    const dadosResposta = await resposta.json()
+    console.log(dadosResposta)
+    if(dadosResposta.sucess) {
+      setUsuario("")
+      setSenha("")
+      setEmail("")
+      setTipo("C")
+    }
+    setMessage(dadosResposta.message)
+  }
 
   return (
     <>
@@ -121,14 +196,71 @@ function TelaAdmin() {
           <br /><br />
 
           <button onClick={realizaCadastro} type="button">Cadastrar</button>
+          <hr />
+          <button onClick={()=> sair(props.setLogado)}>Sair</button>
 
         </div>
+        <hr />
+        <div>
+          <p>{message}</p>
+        </div>
       </section>
+    </>
+  )
+}
+
+function TelaUsuario(props) {
+  const [funcao, setFuncao] = useState("Estoque")
+
+
+  return (
+    <>
+      <h1>Olá, {}</h1>
+
+      <button onClick={()=> setFuncao("Estoque")}>Estoque</button>
+      <br /><br />
+      <button onClick={()=> setFuncao("Caixa")}>Caixa</button> 
+      <hr />
+      <button onClick={()=> sair(props.setLogado)}>Sair</button>
+
+      {(funcao == "Estoque" && <TelaEstoque />)}
+      {(funcao == "Caixa" && <TelaCaixa />)}   
+    
+    
+    </>
+
+  )
+}
+
+function TelaEstoque() {
+
+
+
+  return (
+    <>
+      <h1>Está no modo estoque</h1>
     
     
     
     </>
   )
+}
+
+function TelaCaixa() {
+
+
+
+  return (
+    <>
+      <h1>Está no modo caixa</h1>
+
+    </>
+  )
+
+}
+
+function sair(setLogado) {
+  setLogado(false)
 }
 
 export default App
