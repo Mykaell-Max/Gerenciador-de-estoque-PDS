2eb1edf3d9d11912072bacaec523ac17d58aefe2
Gabriel Kayran <gabrielkayran@gmail.com>
2026-07-19 21:22:59 -0300

    feat: add support for environment variables with python-dotenv integration

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.rotas import auth, usuarios, produtos, logs, movimentacoes
